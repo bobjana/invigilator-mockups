fd4d6040c07fef5116cd11bea5260fea5471bd6b
Dylan Viljoen <dylan.viljoen@nwu.ac.za>
2015-11-24 06:28:27 +0200

    nwu-invigilator-1 Configure basic project * basic spring-boot server app

---
+++ server/src/main/java/za/org/nwu/InvigilatorApplication.java
@@ -0,0 +1,12 @@
+package za.org.nwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvigilatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvigilatorApplication.class, args);
+    }
+}
