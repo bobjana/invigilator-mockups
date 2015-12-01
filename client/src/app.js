5bef556bcc0e54b8bfacd2ecae248724d229b855
Dylan Viljoen <dylan.viljoen@nwu.ac.za>
2015-12-01 06:07:27 +0200

    nwu-invigilator-1 Configure basic project * basic structure * remove unused feautures * introduce exams placeholder

--- client/src/app.js
+++ client/src/app.js
@@ -2,9 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'home'], name: 'home',      moduleId: 'home',      nav: true, title: 'Home' },
+      { route: 'exams',  name: 'exam', moduleId: 'exams/exam', nav: true, title: 'Exams' }
     ]);
 
     this.router = router;
